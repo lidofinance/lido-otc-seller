250033948230945558d45230ad410e1e0a0ba4ec
KRogLA <i@krogla.me>
2022-09-09 20:30:03 +0200

    fix: remove unused files

--- contracts/interfaces/IUniswapRouterV2.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-/// @dev Minified Uniswap V2 router interface
-interface IUniswapRouterV2 {
-    function getAmountsOut(uint256 amountIn, address[] calldata path) external view returns (uint256[] memory amounts);
-
-    function getAmountsIn(uint256 amountOut, address[] calldata path) external view returns (uint256[] memory amounts);
-}
