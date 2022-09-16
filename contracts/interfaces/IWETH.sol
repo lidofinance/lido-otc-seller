1aac0d202968a04a3cbebab58f8b9a4fc6a36b74
Artyom Veremeenko <arwer13@gmail.com>
2022-09-16 17:29:25 +0300

    chore!: remove everything to make a blank state to create PR into

--- contracts/interfaces/IWETH.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-/// @title Minified wrapped ETH interface
-interface IWETH {
-    function deposit() external payable;
-
-    function withdraw(uint256 wad) external;
-}
